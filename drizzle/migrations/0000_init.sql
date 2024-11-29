851ec400145905cf2beb76d34bf74153c3922c8b
dan.castillo <dancastillo99@gmail.com>
2024-11-29 16:42:07 -0500

    fix

--- drizzle/migrations/0000_init.sql
+++ drizzle/migrations/0000_init.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TABLE IF NOT EXISTS "public"."posts" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"title" varchar NOT NULL,
-	"content" text NOT NULL,
+	"content" varchar NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp DEFAULT NULL,
